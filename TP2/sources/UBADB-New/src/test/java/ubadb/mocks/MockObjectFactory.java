25547f474f13e50e547621afc71f2d1415ad7397
Pancho Roslan <pancho@piojo.(none)>
2011-11-12 16:37:11 -0300

    Revert "cambios necesarios para hacer los graficos"
    This reverts commit b6c3e28c32f6dd653e9905c0126db3a982ecda8c.

--- TP2/sources/UBADB-New/src/test/java/ubadb/mocks/MockObjectFactory.java
+++ TP2/sources/UBADB-New/src/test/java/ubadb/mocks/MockObjectFactory.java
@@ -8,11 +8,5 @@ public class MockObjectFactory
 {
 	public static final TableId TABLE_ID = new TableId("a.txt");
 	public static final PageId PAGE_ID = new PageId(0, TABLE_ID);
-	public static final PageId PAGE_ID_1 = new PageId(1, TABLE_ID);
-	public static final PageId PAGE_ID_2 = new PageId(2, TABLE_ID);
-
 	public static final Page PAGE = new Page(PAGE_ID, "abc".getBytes());
-	public static final Page PAGE_1 = new Page(PAGE_ID_1, "abc".getBytes());
-	public static final Page PAGE_2 = new Page(PAGE_ID_2, "abc".getBytes());
-
 }
