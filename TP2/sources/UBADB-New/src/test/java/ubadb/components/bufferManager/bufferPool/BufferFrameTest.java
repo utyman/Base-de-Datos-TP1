d2a530cf9c90b98c63a424023869e68f61d9b435
Hernan Bandura <hbandura@gmail.com>
2011-11-13 15:38:23 -0300

    cleanup

--- TP2/sources/UBADB-New/src/test/java/ubadb/components/bufferManager/bufferPool/BufferFrameTest.java
+++ TP2/sources/UBADB-New/src/test/java/ubadb/components/bufferManager/bufferPool/BufferFrameTest.java
@@ -9,48 +9,53 @@ import org.junit.Test;
 import ubadb.exceptions.BufferFrameException;
 import ubadb.mocks.MockObjectFactory;
 
-public class BufferFrameTest
-{
+public class BufferFrameTest {
 	@Test
-	public void testFrameIsNotDirtyAndWithoutPinsByDefault()
-	{
+	public void testFrameIsNotDirtyAndWithoutPinsByDefault() {
 		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
-		
+
 		assertFalse(bufferFrame.isDirty());
 		assertEquals(0, bufferFrame.getPinCount());
 	}
-	
+
 	@Test
-	public void testCanBeReplacedTrue()
-	{
+	public void testCanBeReplacedTrue() {
 		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
-		
+
 		assertTrue(bufferFrame.canBeReplaced());
 	}
-	
+
 	@Test
-	public void testCanBeReplacedFalse()
-	{
+	public void testCanBeReplacedFalse() {
 		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
 		bufferFrame.pin();
-		
+
 		assertFalse(bufferFrame.canBeReplaced());
 	}
-	
+
 	@Test
-	public void testUnpinPinnedPage() throws Exception
-	{
+	public void testUnpinPinnedPage() throws Exception {
 		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
 		bufferFrame.pin();
 		bufferFrame.pin();
-		
+
 		bufferFrame.unpin();
 		assertEquals(1, bufferFrame.getPinCount());
 	}
 	
-	@Test(expected=BufferFrameException.class)	
-	public void testUnpinUnpinnedPage() throws Exception
-	{
+	public void testReplacePinUnpinnedPage() throws BufferFrameException {
+		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
+		bufferFrame.pin();
+		bufferFrame.pin();
+
+		bufferFrame.unpin();
+		bufferFrame.unpin();
+		assertEquals(0, bufferFrame.getPinCount());
+		assertTrue(bufferFrame.canBeReplaced());
+	}
+
+	@Test(expected = BufferFrameException.class)
+	public void testUnpinUnpinnedPage() throws Exception {
 		BufferFrame bufferFrame = new BufferFrame(MockObjectFactory.PAGE);
 
 		bufferFrame.unpin();
