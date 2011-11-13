dd6b5f15a83e178f9e0608987a1295ecad0bdde2
Hernan Bandura <hbandura@gmail.com>
2011-11-13 13:00:19 -0300

    extracted count buffer frame class

---
+++ TP2/sources/UBADB-New/src/main/java/ubadb/components/bufferManager/bufferPool/replacementStrategies/count/CountBufferFrame.java
@@ -0,0 +1,36 @@
+package ubadb.components.bufferManager.bufferPool.replacementStrategies.count;
+
+import ubadb.common.Page;
+import ubadb.components.bufferManager.bufferPool.BufferFrame;
+
+/**
+ * Keeps track of the total amount of pins made to the buffer frame.
+ * 
+ * @author Grupo9
+ */
+public final class CountBufferFrame extends BufferFrame {
+	
+	/**
+	 * Count strategy reference, used to count page pins.
+	 */
+	private CountReplacementStrategy strategy;
+	
+	/**
+	 * Creates a new CountBufferFrame. 
+	 * 
+	 * @param strategy The CountReplacementStrategy reference.
+	 * @param page The page to hold.
+	 */
+	public CountBufferFrame(CountReplacementStrategy strategy, Page page) {
+		super(page);
+	}
+	
+	/* (non-Javadoc)
+	 * @see ubadb.components.bufferManager.bufferPool.BufferFrame#pin()
+	 */
+	@Override
+	public void pin() {
+		super.pin();
+		strategy.countPin(getPage().getPageId());
+	}
+}
