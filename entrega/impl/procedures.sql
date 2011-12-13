adf889c568fe8f6bd87461a77cf1c0d62cf9d713
Fabian Gonzalez <utyman@gmail.com>
2011-12-12 22:33:39 -0300

    Agregadas las stored procedures y funciones juntas

--- entrega/impl/procedures.sql
+++ entrega/impl/procedures.sql
@@ -1,5 +1,5 @@
 DELIMITER $$
-drop procedure if exists bases.RecorridosTodasRutasUsadas $$
+drop procedure if exists RecorridosTodasRutasUsadas $$
 	CREATE PROCEDURE RecorridosTodasRutasUsadas(IN ano integer)
 		BEGIN
 		       SELECT r
@@ -15,4 +15,64 @@ drop procedure if exists bases.RecorridosTodasRutasUsadas $$
 				       AND v.id_recorrido = r.recorrido)
 			 ) AND ((SELECT count(rut.id_ruta) FROM rutas WHERE rut.id_recorrido = r.id_recorrido) = 1);
 		END $$
+
+drop function if exists `promedioViajesRealizadosPorAno` $$
+CREATE DEFINER=`root`@`localhost` FUNCTION  `promedioViajesRealizadosPorAno`
+								(patente varchar(10)) RETURNS int(11)
+begin
+    DECLARE ano_inicio_servicio int;
+    DECLARE viajes_realizados int;
+    SELECT count(*) into viajes_realizados FROM viajes_realizados 
+    		NATURAL JOIN viajes v WHERE  v.id_vehiculo = patente;
+    SELECT YEAR(fecha_inicio_servicio)  into ano_inicio_servicio 
+    		FROM vehiculos v where v.patente = patente;
+    RETURN (viajes_realizados / (YEAR(NOW()) - ano_inicio_servicio + 1)) ;
+end $$
+
+drop procedure if exists `promedioViajesYEstado` $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE  `promedioViajesYEstado`()
+BEGIN
+		SELECT (SELECT promedioViajesRealizadosPorAno(patente)), situacion from vehiculos;
+		END $$
+
+
+
+DROP PROCEDURE IF EXISTS `ChoferesTodosLosVehiculosUltimoSemestre` $$
+DROP FUNCTION IF EXISTS `agregardosanos` $$
+DROP FUNCTION IF EXISTS `agregarseismeses` $$
+CREATE DEFINER=`root`@`localhost` PROCEDURE  
+		`ChoferesTodosLosVehiculosUltimoSemestre`()
+BEGIN
+		       SELECT *
+	       	       FROM choferes c
+	       	       WHERE NOT EXISTS
+			 (SELECT *
+			  FROM vehiculos v
+			  WHERE (SELECT agregardosanos(v.patente)) > NOW() AND NOT EXISTS (
+				SELECT *
+				 FROM viajes_realizados v1
+				 NATURAL JOIN viajes v2
+				 WHERE (SELECT agregarseismeses(v1.fecha_hora_llegada)) > NOW()
+				       AND v2.id_vehiculo = v.patente
+				 AND EXISTS (
+				      SELECT * FROM asignaciones_chofer WHERE
+					 dni = c.dni AND id_viaje = v2.id_viaje
+				)      
+				) 
+			 );
+		END
+		
+		CREATE DEFINER=`root`@`localhost` FUNCTION  
+		`agregardosanos`(id_vehiculo varchar(10)) RETURNS datetime
+BEGIN
+		return DATE_ADD((SELECT fecha_inicio_servicio FROM vehiculos where patente = id_vehiculo), 
+						INTERVAL 2 YEAR);
+		END
+		
+
+		CREATE DEFINER=`root`@`localhost` FUNCTION  `agregarseismeses`(fecha DATETIME) 
+		RETURNS datetime
+BEGIN
+		return DATE_ADD(fecha, INTERVAL 6 MONTH);
+		END
 
